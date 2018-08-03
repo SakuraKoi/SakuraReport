29c30b22f61d812c72323c2b5ebbdd209abb2075
Ldcr <starsky2013@126.com>
2018-08-03 11:34:37 +0800

    lombok Getter setter

--- src/ldcr/LReport/LReport.java
+++ src/ldcr/LReport/LReport.java
@@ -1,62 +1,72 @@
 package ldcr.LReport;
 
+import lombok.Getter;
+
 import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import ldcr.LReport.Hook.BattlEyeHook;
 import ldcr.LReport.Hook.EmptyHook;
 import ldcr.LReport.Hook.IHook;
+import ldcr.LReport.Hook.MatrixHook;
 import ldcr.LReport.commands.ConsoleReportCommand;
 import ldcr.LReport.commands.ReportCommand;
 import ldcr.LReport.commands.ReportManagerCommand;
 import ldcr.LReport.commands.StaffReportCommand;
-import ldcr.Utils.ExceptionUtils;
-import ldcr.Utils.Bukkit.TitleUtils;
+import ldcr.LReport.messageChannel.DatabaseMessageChannel;
+import ldcr.LReport.messageChannel.IMessageChannel;
+import ldcr.LdcrUtils.plugin.LdcrUtils;
+import ldcr.Utils.exception.ExceptionUtils;
 
-public class Main extends JavaPlugin implements Listener {
-	public static Main instance;
+public class LReport extends JavaPlugin implements Listener {
+	@Getter private static LReport instance;
 	private static CommandSender logger;
 	private String mysqlServer;
 	private String mysqlPort;
 	private String mysqlDatabase;
 	private String mysqlUser;
 	private String mysqlPassword;
-	public String displayServer;
-	public String serverID;
-	public ReportManager manager;
-	public MessageChannel messageChannel;
+	public static String displayServer;
+	public static String serverID;
+	@Getter private ReportManager reportManager;
+	@Getter private IMessageChannel messageChannel;
 	public ManageGUICreator guiCreator;
-	public IHook battlEyeHook;
+	public IHook matrixHook;
 	public SpecListener specListener;
 	@Override
 	public void onEnable() {
 		instance = this;
+		LdcrUtils.requireVersion(31);
 		logger = Bukkit.getConsoleSender();
 		loadConfig();
 		specListener = new SpecListener();
-		manager = new ReportManager();
+		reportManager = new ReportManager();
 		try {
-			manager.connect(mysqlServer,mysqlPort,mysqlDatabase,mysqlUser,mysqlPassword);
+			reportManager.connect(mysqlServer,mysqlPort,mysqlDatabase,mysqlUser,mysqlPassword);
 		} catch (final SQLException e) {
-			ExceptionUtils.printStacetrace(e);
-			Main.sendConsoleMessage("&c数据库连接失败, 请检查配置文件.");
+			ExceptionUtils.printStacktrace(e);
+			LReport.sendConsoleMessage("&c数据库连接失败, 请检查配置文件.");
 			setEnabled(false);
 			return;
 		}
-		messageChannel = new MessageChannel();
+		messageChannel = new DatabaseMessageChannel(reportManager.getDataSource());
+		try {
+			messageChannel.connectChannel();
+		} catch (final IOException e) {
+			ExceptionUtils.printStacktrace(e);
+			LReport.sendConsoleMessage("&c广播信道连接失败");
+		}
 		getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
-		getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", messageChannel);
-		hookBattlEye();
+		hookMatrix();
 		guiCreator = new ManageGUICreator();
 		Bukkit.getPluginManager().registerEvents(new ManageGUIListener(), this);
 		Bukkit.getPluginManager().registerEvents(specListener, this);
@@ -70,8 +80,15 @@ public class Main extends JavaPlugin implements Listener {
 	}
 	@Override
 	public void onDisable() {
-		manager.stopConnection();
-		specListener.backAllSpectator();
+		if (reportManager != null) {
+			reportManager.stopConnection();
+		}
+		if (messageChannel != null) {
+			messageChannel.disconnectChannel();
+		}
+		if (specListener != null) {
+			specListener.backAllSpectator();
+		}
 	}
 	public void loadConfig() {
 		final File configFile = new File(getDataFolder(),"config.yml");
@@ -89,28 +106,15 @@ public class Main extends JavaPlugin implements Listener {
 		serverID = config.getString("BungeeServerID","undefined");
 		sendConsoleMessage("&a此子服被命名为 "+displayServer+" ["+serverID+"]");
 	}
-	private void hookBattlEye() {
-		if (Bukkit.getPluginManager().isPluginEnabled("BattlEye")) {
-			sendConsoleMessage("&bBattlEye 反作弊系统已挂钩");
-			battlEyeHook = new BattlEyeHook();
+	private void hookMatrix() {
+		if (Bukkit.getPluginManager().isPluginEnabled("Matrix-AntiKillaura")) {
+			sendConsoleMessage("&bMatrix-AntiKillaura 已挂钩");
+			matrixHook = new MatrixHook();
 		} else {
-			battlEyeHook = new EmptyHook();
+			matrixHook = new EmptyHook();
 		}
 	}
-	public static void boardcastOP() {
-		try {
-			if (!Main.instance.manager.hasReport()) return;
-		} catch (final SQLException ex) {
-			ExceptionUtils.printStacetrace(ex);
-			sendConsoleMessage("&c错误: 数据库操作出错, 请检查后台报错.");
-			return;
-		}
-		for (final Player p : Bukkit.getOnlinePlayers()) {
-			if (p.hasPermission("lreport.lpt")) {
-				TitleUtils.sendTitle(p, "", "§c你有未处理举报", 1, 20, 1);
-			}
-		}
-	}
+
 	public static void sendConsoleMessage(final String... messages) {
 		for (final String str : messages) {
 			logger.sendMessage("§a§lLReport §7>> §e"+str.replace('&', '§').replace("§§", "&"));
@@ -120,36 +124,17 @@ public class Main extends JavaPlugin implements Listener {
 	@EventHandler
 	public void onJoin(final PlayerLoginEvent e) {
 		Bukkit.getScheduler().runTaskLaterAsynchronously(this, new Runnable() {
-
 			@Override
 			public void run() {
 				try {
-					Main.instance.manager.playerOnline(e.getPlayer().getName());
+					LReport.instance.reportManager.playerOnline(e.getPlayer().getName());
 				} catch (final SQLException e) {
-					ExceptionUtils.printStacetrace(e);
+					ExceptionUtils.printStacktrace(e);
 					sendConsoleMessage("&c错误: 发生数据库错误, 请检查后台报错.");
 				}
 			}
 
 		}, 10);
-
-		if (e.getPlayer().hasPermission("lreport.lpt")) {
-			Bukkit.getScheduler().runTaskLaterAsynchronously(this, new Runnable() {
-				@Override
-				public void run() {
-					try {
-						if (manager.hasReport()) {
-							TitleUtils.sendTitle(e.getPlayer(), "", "§c你有未处理举报", 1, 20, 1);
-						}
-					}catch (final SQLException ex) {
-						ExceptionUtils.printStacetrace(ex);
-						sendConsoleMessage("&c错误: 发生数据库错误, 请检查后台报错.");
-						return;
-					}
-				}
-			}
-			, 100);
-		}
 	}
 	@EventHandler
 	public void onLogout(final PlayerQuitEvent e) {
@@ -157,9 +142,9 @@ public class Main extends JavaPlugin implements Listener {
 			@Override
 			public void run() {
 				try {
-					Main.instance.manager.playerOffline(e.getPlayer().getName());
+					LReport.instance.reportManager.playerOffline(e.getPlayer().getName());
 				} catch (final SQLException e) {
-					ExceptionUtils.printStacetrace(e);
+					ExceptionUtils.printStacktrace(e);
 					sendConsoleMessage("&c错误: 发生数据库错误, 请检查后台报错.");
 				}
 			}
@@ -169,10 +154,10 @@ public class Main extends JavaPlugin implements Listener {
 		@Override
 		public void run() {
 			try {
-				Main.instance.manager.updatePlayerlist();
+				LReport.instance.reportManager.updatePlayerlist();
 			} catch (final SQLException e) {
-				ExceptionUtils.printStacetrace(e);
-				Main.sendConsoleMessage("&c更新玩家列表时发生了数据库错误.");
+				ExceptionUtils.printStacktrace(e);
+				LReport.sendConsoleMessage("&c更新玩家列表时发生了数据库错误.");
 			}
 		}
 
